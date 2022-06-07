28e3967ce6456d704ccaa7eb6cee395a549839e3
Rushi <44952533+Skyliegirl33@users.noreply.github.com>
2022-06-07 05:05:54 +0200

    Animation and tileset update

--- source/Player.cpp
+++ source/Player.cpp
@@ -18,8 +18,7 @@
 
 #include "Player.h"
 
-Player::Player() : x(20), y(20), direction(1), currentFrame(0), currentTime(0), state(State::Idle)
-{
+Player::Player() : x(20), y(20), direction(1), state(State::Idle) {
     playerSprite = {
         Sprite{
             .id = addSprite(x, y, 0)
@@ -32,39 +31,40 @@ Player::Player() : x(20), y(20), direction(1), currentFrame(0), currentTime(0), 
 
     Animation idleu = Animation{
         .frames = {
-            { playerSprite[0].id, {Frame{.tileIndex = 80} } },
-            { playerSprite[1].id, {Frame{.tileIndex = 84} } }
+            { Frame{.tileIndex = 80} },
+            { Frame{.tileIndex = 84} } 
         },
         .length = 1
     };
 
     Animation idled = Animation{
         .frames = {
-            { playerSprite[0].id, {Frame{.tileIndex = 0} } },
-            { playerSprite[1].id, {Frame{.tileIndex = 4} } }
+            { Frame{.tileIndex = 0} },
+            { Frame{.tileIndex = 4} }
         },
         .length = 1
     };
 
     Animation idlel = Animation{
         .frames = {
-            { playerSprite[0].id, {Frame{.tileIndex = 40} } },
-            { playerSprite[1].id, {Frame{.tileIndex = 44} } }
+            { Frame{.tileIndex = 40} },
+            { Frame{.tileIndex = 44} }
         },
         .length = 1
     };
 
     Animation idler = Animation{
         .frames = {
-            { playerSprite[0].id, { Frame{.tileIndex = 40} } },
-            { playerSprite[1].id, { Frame{.tileIndex = 44} } }
+            { Frame{.tileIndex = 40} },
+            { Frame{.tileIndex = 44} }
         },
         .length = 1
     };
 
     Animation walkd = Animation{
         .frames = {
-            { playerSprite[1].id, { Frame{.tileIndex = 8}, Frame{.tileIndex = 12}, Frame{.tileIndex = 16}, Frame{.tileIndex = 20} } }
+            { Frame{.tileIndex = 0} },
+            { Frame{.tileIndex = 8}, Frame{.tileIndex = 12}, Frame{.tileIndex = 16}, Frame{.tileIndex = 20} }
         },
         .length = 4
     };
@@ -79,13 +79,11 @@ Player::Player() : x(20), y(20), direction(1), currentFrame(0), currentTime(0), 
     changeAnim(PlayerAnimation::IDLE_DOWN);
 }
 
-void Player::update()
-{
+void Player::update() {
     playCurrentAnim(*currentAnim);
 }
 
-void Player::movePlayer(int16_t x, int16_t y)
-{
+void Player::movePlayer(int16_t x, int16_t y) {
     if (currentAnim == nullptr)
         return;
     // bleh, maybe have an array for idle/walk animations
@@ -113,10 +111,7 @@ void Player::movePlayer(int16_t x, int16_t y)
     this->y = y;
 }
 
-void Player::inputEvent(InputContext &ctx)
-{
-    // const InputContext& ctx = event.getContext();
-
+void Player::inputEvent(InputContext &ctx) {
     // iprintf("\x1b[0;0HDir: %i", direction);
 
     // Pause game if action is triggered
@@ -152,54 +147,53 @@ void Player::inputEvent(InputContext &ctx)
     }
 }
 
-void Player::setFrame(u8 sprite, u8 newFrame)
-{
+void Player::setFrame(u8 sprite, u8 tileIndex) {
     if (currentAnim) {
-        auto found = currentAnim->frames.find(sprite);
-
-        if (found != currentAnim->frames.end() && newFrame < found->second.size())
-            setSpriteTiles(sprite, found->second[newFrame].tileIndex);
+        setSpriteTiles(sprite, tileIndex);
     }
 }
 
-void Player::pushAnim(const PlayerAnimation &name, const Animation &anim)
-{
+void Player::pushAnim(const PlayerAnimation &name, const Animation &anim) {
     animations[name] = anim;
 }
 
 void Player::playCurrentAnim(const Animation &anim) {
-    ++currentTime;
-
     for (u8 i = 0; i < SPRITES; i++) {
+        ++playerSprite[i].currentTime;
+
         u8 spriteId = playerSprite[i].id;
-        auto found = anim.frames.find(spriteId);
+        auto& frames = anim.frames[i];
 
-        if (found != anim.frames.end() && currentTime >= found->second[currentFrame].frameTime)
-        {
-            currentTime = 0;
+        if (playerSprite[i].currentTime >= frames[playerSprite[i].currentFrame].frameTime) {
+            playerSprite[i].currentTime = 0;
 
-            if (currentFrame + 1 < anim.length)
-                ++currentFrame;
+            if (playerSprite[i].currentFrame + 1 < frames.size())
+                ++playerSprite[i].currentFrame;
             else
-                currentFrame = 0; // TODO: add support for non looping animations
+                playerSprite[i].currentFrame = 0; // TODO: add support for non looping animations
 
-            setFrame(spriteId, currentFrame);
+            if (frames[playerSprite[i].currentFrame].tileIndex == 255)
+                continue;
+
+            iprintf("\x1b[0;0HIdx: %i", frames[playerSprite[i].currentFrame].tileIndex);
+
+            setFrame(spriteId, frames[playerSprite[i].currentFrame].tileIndex);
         }
     }
 }
 
-void Player::changeAnim(const PlayerAnimation &name)
-{
+void Player::changeAnim(const PlayerAnimation &name) {
     Animation &animation = animations[name];
 
     if (((currentAnim && currentAnim != &animation) || (currentAnim != &animation)) && animation.length != 0)
     {
         currentAnim = &animation;
-        currentFrame = 0;
+        for (u8 i = 0; i < SPRITES; i++) {
+            playerSprite[i].currentFrame = 0;
+        }
     }
 }
 
-void Player::changeAnim(u8 animIndex)
-{
+void Player::changeAnim(u8 animIndex) {
     changeAnim(static_cast<PlayerAnimation>(animIndex));
 }
